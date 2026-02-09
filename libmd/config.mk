ceb2b9ab63e0cd3e027b828ac2254ff7a4199088
YSergei Solomatov <solomatovs@gmail.com>
2026-02-09 16:19:50 +0300

    add: libmd project

---
+++ libmd/config.mk
@@ -0,0 +1,5 @@
+# config.mk — настройки по умолчанию
+# Переопределить: создать config.local.mk или задать переменные окружения.
+# Подробнее: см. config.local.mk.example
+
+HADRONS_MIRROR ?= https://archive.hadrons.org
