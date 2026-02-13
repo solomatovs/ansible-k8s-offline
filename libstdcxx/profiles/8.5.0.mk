80f1d3fe006c3bd2cb5fe3ba167a07d98401305a
YSergei Solomatov <solomatovs@gmail.com>
2026-02-13 21:21:40 +0300

    tmp save

--- libstdcxx/profiles/8.5.0.mk
+++ libstdcxx/profiles/8.5.0.mk
@@ -1,5 +1,6 @@
 PF_VERSION    = 8.5.0
 PF_DOCKERFILE = Dockerfile
+PF_DEPS       = toolchain-gcc:8.5.0
 
 PF_SRC_URL    = $(LIBSTDCXX_SRC_URL)/gcc-$(PF_VERSION)/gcc-$(PF_VERSION).tar.xz
 PF_SRC_FILE   = gcc-$(PF_VERSION).tar.xz
